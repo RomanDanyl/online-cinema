e717457005ca2fc29ded7309d1007db7d8b46585
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2025-11-30 19:18:25 +0200

    Delete unused models & migrations

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -10,16 +10,7 @@ from database.models.accounts import (
     RefreshTokenModel,
     UserProfileModel
 )
-from database.models.movies import (
-    MovieModel,
-    LanguageModel,
-    ActorModel,
-    GenreModel,
-    CountryModel,
-    MoviesGenresModel,
-    ActorsMoviesModel,
-    MoviesLanguagesModel
-)
+
 from database.session_sqlite import reset_sqlite_database as reset_database
 from database.validators import accounts as accounts_validators
 
