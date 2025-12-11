926f308d953e0f792a7bd1fa8e0dbf60b179a350
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2025-12-11 17:16:38 +0200

    Create schemas for movie part

---
+++ src/schemas/movies.py
@@ -0,0 +1,71 @@
+import uuid
+from datetime import date
+from decimal import Decimal
+from enum import Enum
+from typing import List, Optional
+
+from pydantic import BaseModel, Field
+
+
+class UserReactionsEnum(str, Enum):
+    LIKE: str = "like"
+    DISLIKE: str = "dislike"
+
+
+class GenreSchema(BaseModel):
+    id: int
+    name: str
+
+    model_config = {"from_attributes": True}
+
+
+class MovieDirectorSchema(BaseModel):
+    id: int
+    name: str
+
+    model_config = {"from_attributes": True}
+
+
+class MovieStarSchema(BaseModel):
+    id: int
+    name: str
+
+    model_config = {"from_attributes": True}
+
+
+class MovieItemSchema(BaseModel):
+    id: int
+    uuid: uuid.UUID
+
+    name: str
+    year: int
+    time: int
+    imdb: float
+    votes: int
+    price: Decimal
+    genres: List[GenreSchema]
+    stars: List[MovieStarSchema]
+    directors: List[MovieDirectorSchema]
+
+    model_config = {"from_attributes": True}
+
+
+class MovieListItemSchema(MovieItemSchema):
+    avg_rating: float
+    ratings_count: int
+    likes_count: int
+    comments_count: int
+
+    is_favorite: Optional[bool]
+    user_rating: Optional[int]
+
+    user_reaction: Optional[UserReactionsEnum]
+
+    model_config = {"from_attributes": False}
+
+
+class MovieListResponseSchema(BaseModel):
+    items: List[MovieItemSchema]
+    total: int
+    page: int
+    page_size: int
