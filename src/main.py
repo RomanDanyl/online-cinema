c70a0cb923bb2af4bee6a125b83c651b7a95fa6f
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2025-12-31 18:41:02 +0200

    Include new routers to main router

--- src/main.py
+++ src/main.py
@@ -1,13 +1,34 @@
 from fastapi import FastAPI
 
-from routes import accounts_router, movies_router, cart_router, orders_router, payments_router
+from routes import (
+    accounts_router,
+    movies_public_router,
+    movies_admin_router,
+    cart_router,
+    orders_router,
+    payments_router,
+)
 
 app = FastAPI(title="Online Cinema API")
 
 api_version_prefix = "/api/v1"
 
-app.include_router(accounts_router, prefix=f"{api_version_prefix}/accounts", tags=["accounts"])
-app.include_router(movies_router, prefix=f"{api_version_prefix}/movies", tags=["movies"])
+app.include_router(accounts_router, prefix=api_version_prefix, tags=["accounts"])
+app.include_router(
+    movies_public_router,
+    prefix=api_version_prefix,
+    tags=["Movies"],
+)
+
+app.include_router(
+    movies_admin_router,
+    prefix=api_version_prefix,
+    tags=["Admin: Movies"],
+)
 app.include_router(cart_router, prefix=f"{api_version_prefix}/cart", tags=["cart"])
-app.include_router(orders_router, prefix=f"{api_version_prefix}/orders", tags=["orders"])
-app.include_router(payments_router, prefix=f"{api_version_prefix}/payments", tags=["payments"])
+app.include_router(
+    orders_router, prefix=f"{api_version_prefix}/orders", tags=["orders"]
+)
+app.include_router(
+    payments_router, prefix=f"{api_version_prefix}/payments", tags=["payments"]
+)
