f99ec4ed53b4bba845c487e6e8f79afb03ed7e20
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2026-01-14 18:03:24 +0200

    Add schemas for Cart, CartItem

---
+++ src/schemas/cart.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from decimal import Decimal
+
+from pydantic import BaseModel, Field, ConfigDict
+
+
+class CartItemCreateSchema(BaseModel):
+    movie_id: int = Field(..., ge=1)
+
+
+class CartMovieSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    name: str
+    year: int
+    price: Decimal
+    genres: list[str]
+
+
+class CartItemSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    added_at: datetime
+    movie: CartMovieSchema
+
+
+class CartListResponseSchema(BaseModel):
+    items: list[CartItemSchema]
+
+
+class CartItemResponseSchema(BaseModel):
+    item: CartItemSchema
+
+
+class CartActionResponseSchema(BaseModel):
+    message: str
